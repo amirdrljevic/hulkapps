ff24cca47489ee32c2d35957fcaba71e0e55fa81
amirdrljevic <amirdrljevic@gmail.com>
2023-08-20 22:43:01 +0200

    Add fake data (#4)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Generate some users
+5.times do |n|
+    email = "user-#{n+1}@mail.com"
+    first_name = Faker::Name.first_name
+    last_name  = Faker::Name.last_name
+    password   = "lozinka"
+    User.create!( email: email, 
+                  first_name: first_name,
+                  last_name: last_name,
+                  password: password,
+                  password_confirmation: password)
+    end              
+  
+    # Generate posts for these random users
+    users = User.order(:created_at).take(5)
+    50.times do
+      title       = Faker::Lorem.sentence(word_count: 3)
+      description = Faker::Lorem.paragraph(sentence_count: 30)
+      users.each { |user| 
+        user.posts.create!(title: title, 
+                           description: description)}
+    end
+  
+    # Generate comments for some posts
+    posts = Post.order(:created_at).take(10)
+    5.times do 
+      content = Faker::Lorem.sentence(word_count: 5)
+      posts.each { |post| post.comments.create!(user_id: 1,
+                                                post_id: post,
+                                                comment: content) }
+    end
